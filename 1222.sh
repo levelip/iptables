f98328bc273f06ba2dd2cfd58e997d6c935dad3b
levelip <levelip@users.noreply.github.com>
2015-02-17 11:33:42 +0800

    Delete 1222.sh

--- 1222.sh
+++
@@ -1,5 +0,0 @@
-mkdir /tmp/conf$(date +"%Y%m%d")-backup
-cd  /tmp/conf$(date +"%Y%m%d")-backup
-tar jcvf conf.tar.bz2 /usr/local/nginx/conf
-sh /web/dropbox_uploader.sh upload /tmp/conf$(date +"%Y%m%d")-backup /conf$(date +"%Y%m%d")
-rm -rf /tmp/conf$(date +"%Y%m%d")-backup
